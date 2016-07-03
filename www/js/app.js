b3242223f3aeb7e7d6bd1b0d8b5af607aa8eff5a
Francisco Avello Barrera <francisco.avello@usach.cl>
2016-07-03 17:36:00 -0400

    Añadido soporte cámara y geolocalización

--- www/js/app.js
+++ www/js/app.js
@@ -3,24 +3,41 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-var myApp = angular.module('kimen', ['ionic', 'ngCordova', 'ui.router', 'kimen.controllers'])
+var myApp = angular.module('kimen', ['ionic', 'ngCordova','IonicitudeModule','controladoresKimen'])
 
-myApp.run(function($ionicPlatform) {
+myApp.run(function($ionicPlatform, Ionicitude) {
   $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
 
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
   });
 })
 
 myApp.config(function($stateProvider, $urlRouterProvider) {
-  $urlRouterProvider.otherwise("/404");
   $stateProvider
   .state('root', {
     url: "",
     templateUrl: "views/vistaPrincipal.html",
   })
-  .state('vistaMapa', {
-    url: "/vistaMapa",
-    templateUrl: "views/vistaMapa.html",
+  .state('vistaAPP', {
+    url: "/vistaAPP",
+    templateUrl: "views/vistaAPP.html"      
+  })
+  .state('vistaAPP.datosLoc', {
+    url: '/datosLoc',
+    views: {
+      'vistaAPPPrincipal': {
+        templateUrl: 'views/datosLoc.html'
+      }
+    }
   })
   .state('nuevoRegistro', {
     url: "/nuevoRegistro",
@@ -30,6 +47,5 @@ myApp.config(function($stateProvider, $urlRouterProvider) {
   	url: '/404',
     templateUrl: 'views/404.html'
   })
+  $urlRouterProvider.otherwise("/404");
 });
-
-
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,2 +1,31 @@
-angular.module('kimen.controllers', ['ionic', 'ngCordova'])
-	.controller()
+var myApp = angular.module('controladoresKimen', ['ionic', 'ngCordova'])
+
+myApp.controller('CameraCtrl', function($scope, $cordovaCamera, $cordovaGeolocation,$location, $rootScope, $timeout){
+	$rootScope.latitud = '';
+	$rootScope.longitud = '';
+	$scope.takePicture = function(){
+		$cordovaCamera.getPicture({})
+			.then(function(data){
+
+			}, function(error){
+
+			});
+	};	
+
+	$scope.obtenerLocalizacion = function(){
+		var posOptions = {timeout: 10000, enableHighAccuracy: true};
+		$cordovaGeolocation
+		    .getCurrentPosition(posOptions)
+		    .then(function (position) {
+		      var lat  = position.coords.latitude;
+		      var long = position.coords.longitude;
+		      $rootScope.latitud = lat;
+		      $rootScope.longitud = long;
+		      console.log($rootScope.latitud);
+		      console.log($rootScope.longitud);
+		      $location.path('/vistaAPP/datosLoc');
+		    }, function(err) {
+		      // error
+		    });
+	};	
+})
